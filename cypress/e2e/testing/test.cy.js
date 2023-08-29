80c3b9451693e263741443af1029f8b044898301
marcalderon22 <mcalderon@musclepoints.com>
2023-08-29 11:25:05 -0600

    [update] config yaml

---
+++ cypress/e2e/testing/test.cy.js
@@ -0,0 +1,27 @@
+
+
+describe('Test Click', () => {
+    beforeEach(() => {
+        cy.visit('www.google.com');
+    });
+    
+    it('click and search', () => {
+        cy.get("#APjFqb").click();
+        cy.get("#APjFqb").type("muscle points costa rica");
+        cy.wait(1000);
+        cy.get(".gNO89b").first().click();
+        cy.wait(5000);
+        cy.contains('.LC20lb.MBeuO.DKV0Md', 'Muscle Points').click();
+
+        cy.wait(5000);
+
+        cy.origin('https://www.musclepoints.com', () => {
+            cy.get('h1.heading').first().should('have.text', 'Where banks run their rewards');
+        });
+
+       
+
+    });
+
+    
+});
