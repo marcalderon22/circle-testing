59228f4b8ab1b3c79a03cee404a83a74773acac2
marcalderon22 <140531153+marcalderon22@users.noreply.github.com>
2023-09-26 11:23:42 -0600

    Update test.cy.js

--- cypress/e2e/testing/test.cy.js
+++ cypress/e2e/testing/test.cy.js
@@ -5,7 +5,7 @@ describe('Test Click', () => {
         cy.visit('www.google.com');
     });
     
-    it('click and search', () => {
+    it('click and search muscle', () => {
         cy.get("#APjFqb").click();
         cy.get("#APjFqb").type("muscle points costa rica");
         cy.wait(1000);
@@ -18,9 +18,21 @@ describe('Test Click', () => {
         cy.origin('www.musclepoints.com', () => {
             cy.get('h1.heading').first().should('have.text', 'Not Found');
         });
+    });
+    
+    it('click and search google', () => {
+        cy.get("#APjFqb").click();
+        cy.get("#APjFqb").type("muscle points costa rica");
+        cy.wait(1000);
+        cy.get(".gNO89b").first().click();
+        cy.wait(5000);
+        cy.contains('.LC20lb.MBeuO.DKV0Md', 'Muscle Points').click();
 
-       
+        cy.wait(5000);
 
+        cy.origin('www.musclepoints.com', () => {
+            cy.get('h1.heading').first().should('have.text', 'Where banks run their rewards');
+        });
     });
 
     
